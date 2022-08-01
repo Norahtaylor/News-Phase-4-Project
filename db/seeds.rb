2a05bcacb62df7483b2892e3d019196662318759
Subhan Khan <subhank1997@gmail.com>
2022-08-01 13:07:17 -0400

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,6 @@
 require 'faker'
 
-Article.create(title: "Land a job", author: "Norah", description: "This is an article to help us get a job", image: "", category: "Tech")
+puts "🌱 Seeding spices..."
 
-User.create(first_name: "John", last_name: "Tony", username: "Johnnyboy")
-User.create(first_name:Faker::name.first_name, last_name:Faker::name.last_name, username:Faker.name::username)
+
+puts "✅ Done seeding!"
