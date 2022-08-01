31a3eee968b59f03b5b8abad82061c8af0cfd621
[Norah Taylor] <emt368@gmail.com>
2022-08-01 13:04:18 -0400

    seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,21 @@
 require 'faker'
 
-Article.create(title: "Land a job", author: "Norah", description: "This is an article to help us get a job", image: "", category: "Tech")
+# Article.create(title: "Land a job", author: "Norah", description: "This is an article to help us get a job", image: "", category: "Tech")
 
-User.create(first_name: "John", last_name: "Tony", username: "Johnnyboy")
-User.create(first_name:Faker::name.first_name, last_name:Faker::name.last_name, username:Faker.name::username)
+# User.create(first_name: "John", last_name: "Tony", username: "Johnnyboy")
+# 50.times do
+#     first_name = Faker::Name.unique.name
+#     last_name= Faker::Name.unique.name
+#     username= Faker::Name.unique.name
+#         # name = Faker::Name.unique.name
+#     User.create(username: username, first_name: first_name, last_name:last_name)
+ 
+# end 
+
+50.times do 
+    title = Faker::Marketing.buzzwords
+    author= Faker::Name.unique.name 
+    description=Faker::Quote.most_interesting_man_in_the_world
+    published_at= Faker::Date.between(from: '2001-09-23', to: '2022-08-01')
+    Article.create(title: title, author: author, description: description, published_at: published_at)
+end 
