a5cf0eaef71f861a30848e9bfad62e17040b30a5
[Norah Taylor] <emt368@gmail.com>
2022-08-03 11:49:42 -0400

    css, blog

--- my-app/src/components/Homepage.js
+++ my-app/src/components/Homepage.js
@@ -5,19 +5,10 @@ import Button from 'react-bootstrap/Button';
 
 function Homepage({ articleList, image, title, description, date }) {
   return(
-    <div className='homePageCard'>
-    <Card className="card" style={{ width: '15rem' }}>
-      {articleList}
-    <Card className="news-img">
-      <Card.Img variant="top" src={image} />
-    </Card>
-    <Card.Body className="container">
-      <Card.Title>{title}</Card.Title>
-      <Card.Text>{description}</Card.Text>
-      <Button variant="primary">Add To Reading List</Button>
-    </Card.Body>
-  </Card>
-    </div>
+  
+
+    
+  
   )
 }
 
