db3f05498c9cb31ada7de974c408aefa096d0bdb
[Norah Taylor] <emt368@gmail.com>
2022-08-04 10:04:46 -0400

    added controllers back in

--- my-app/src/components/FirstScreen.js
+++ my-app/src/components/FirstScreen.js
@@ -2,7 +2,16 @@ import React from 'react'
 
 function FirstScreen() {
   return (
-    <div>FirstScreen</div>
+    <div>FirstScreen
+      {/* Container for the image gallery */}
+      <div class="container">
+
+       {/* Full-width images with number text  */}
+        <div class="mySlides">
+          <div class="numbertext">1 / 6</div>
+          <img src="img_woods_wide.jpg" style="width:100%"/>
+        </div>
+    </div>
   )
 }
 
